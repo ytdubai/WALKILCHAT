27814e01652b043b9f2fcd3f300371b5b61345c7
ytdubai <ytmesfin@icloud.com>
2026-02-10 13:03:06 +0400

    fix: set implicit flowType in Supabase client

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -7,6 +7,7 @@ export const supabase = createClient(supabaseUrl, supabaseKey, {
   auth: {
     autoRefreshToken: true,
     persistSession: true,
-    detectSessionInUrl: true
+    detectSessionInUrl: true,
+    flowType: 'implicit'
   }
 })
